0499a386f6d57fc82f2582f4b175f7a4ec56133a
Mārtiņš Kristaps Mickus <64271878+MartinsKMickus@users.noreply.github.com>
2023-05-23 21:04:42 +0300

    Page 2 LV

--- js/translations/page_2.js
+++ js/translations/page_2.js
@@ -2,7 +2,7 @@ const translations = {
     en: {
       title_1: `Through The Door`,
       text_1: `I don't live close to city center but there is a bus stop right next to my home.
-                Although I live close to the bus stop I constantly miss it and then only option is to walk to the next stop which is at least 10 times further away.`,
+                Although I live close to the bus stop I constantly miss it and then only option is to walk to the next stop which is at least 20 times further away.`,
       text_2: `It is pretty hard to get up on time because you probably would like to spend more time in bed rather than to do something more productive.
                 After some time though I am up and running just to not miss my bus.
                 Coffee is also a very great thing that makes me just stop and make it, I usually use moka pot to make my coffee nowadays because it tastes fantastic.
@@ -14,8 +14,13 @@ const translations = {
     },
     lv: {
       title_1: 'Caur Durvīm',
-      text_1: 'Pirmais teksts',
-      text_2: 'Otrais teksts',
+      text_1: `Es nedzīvoju tuvu pilsētas centram, bet autobusa pietura ir tuvu manām mājām.
+                Kaut gan pietura ir tuvu, es bieži autobusu nokavēju un tad vienīgā izvēle ir iet līdz nākamai pieturai, kas ir vismaz 20 reizes tālāk.`,
+      text_2: `Ir diezgad sarežģīti piecelties laikā, jo parasti jau gribas pavadīt ilgāku laiku gultā, nekā darīt ko produktīvu.
+                Tomēr pēc kāda laika esmu augšā un skrienu, lai nenokavētu autobusu.
+                Kafija ir viena lieliska lieta, kas liek man apstāties un to uztaisīt, tai parasti izmantoju mokas pagatavotāju, jo tad tā garšo pasakaini.
+                Tieši tā pēc sēdēšanas pie galda, es pamanu ka autobus atiet pēc piecām minūtēm un es nepagūšu uz to laikā.`,
+      text_3: `Pēc izlietas kafijas autobus ir nokavēts. Tagad es skrienu ārā caur durvīm, lai paspētu līdz nākamai pieturai.`,
       btn_back: 'Solis Atpakaļ',
       btn_home: 'Uz Mājām',
       btn_continue: 'Turpināt'
