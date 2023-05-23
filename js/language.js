9a5283dd5b73eac17161f779325ddc4e2d460f63
Mārtiņš Kristaps Mickus <64271878+MartinsKMickus@users.noreply.github.com>
2023-05-23 16:39:47 +0300

    Language change implemented

---
+++ js/language.js
@@ -0,0 +1,47 @@
+// Function to change the language
+function changeLanguage(lang) {
+    const translation = translations[lang];
+
+    // document.getElementById('title_1').textContent = translation.title_1;
+    // document.getElementById('image').setAttribute('alt', translation.altText);
+    // document.getElementById('lang-btn-en').textContent = translation.langBtnEn;
+    // document.getElementById('lang-btn-es').textContent = translation.langBtnEs;
+
+    for (const translationId in translations[lang]) {
+        // console.log(translationId)
+        document.getElementById(translationId).textContent = translations[lang][translationId];
+        // const translation = translations[currentLanguage][translationId];
+    }
+
+    // Active state of buttons
+    const langBtns = document.getElementsByClassName('lang-btn');
+    for (let i = 0; i < langBtns.length; i++) {
+        langBtns[i].classList.remove('active');
+    }
+    document.getElementById(`lang-btn-${lang}`).classList.add('active');
+
+    // Save the selected language to local storage
+    localStorage.setItem('selectedLanguage', lang);
+}
+
+// Check if a selected language is already stored
+const storedLanguage = localStorage.getItem('selectedLanguage');
+if (storedLanguage) {
+    // Use the stored language
+    changeLanguage(storedLanguage);
+}
+
+// Event listeners for language buttons
+document.getElementById('lang-btn-en').addEventListener('click', function (event) {
+    // Don't scroll to top
+    event.preventDefault();
+
+    changeLanguage('en');
+});
+
+document.getElementById('lang-btn-lv').addEventListener('click', function (event) {
+    // Don't scroll to top
+    event.preventDefault();
+
+    changeLanguage('lv');
+});
---
+++ js/translations/page_3.js
@@ -0,0 +1,22 @@
+const translations = {
+    en: {
+      title_1: `In The Motion`,
+      text_1: `There are a lot of options in our lifes and it is hard to choose the right one that suits you.
+                I have chosen this path to learn new things and try to find my goal.
+                The fascinating technologies have lured me in this journey, to explore this simulation, to explore life.
+                Although there are also some things and places that make me stop on the way and think about constant change in everything around us.
+                Either it is a hard Hackintosh installation that I work with till the first light in the morning
+                or maybe it is my home garden or any other hobby that is stopping me.
+                The real journey is more like a path that brings me to the university, this is something what the story is about.`,
+      btn_back: 'Step Back',
+      btn_home: 'To Home',
+      btn_continue: 'Continue'
+    },
+    lv: {
+      title_1: 'Kustībā',
+      text_1: 'Pirmais teksts',
+      btn_back: 'Solis Atpakaļ',
+      btn_home: 'Uz Mājām',
+      btn_continue: 'Turpināt'
+    }
+  };
