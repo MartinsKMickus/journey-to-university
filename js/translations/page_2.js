7351b199dacff040743f137451aa9fdee5fa15c1
Mārtiņš Kristaps Mickus <64271878+MartinsKMickus@users.noreply.github.com>
2023-05-23 17:21:47 +0300

    Text appears

--- js/translations/page_2.js
+++ js/translations/page_2.js
@@ -1,13 +1,13 @@
 const translations = {
     en: {
       title_1: `Through The Door`,
-      text_1: `There are a lot of options in our lifes and it is hard to choose the right one that suits you.
-                I have chosen this path to learn new things and try to find my goal.
-                The fascinating technologies have lured me in this journey, to explore this simulation, to explore life.
-                Although there are also some things and places that make me stop on the way and think about constant change in everything around us.
-                Either it is a hard Hackintosh installation that I work with till the first light in the morning
-                or maybe it is my home garden or any other hobby that is stopping me.
-                The real journey is more like a path that brings me to the university, this is something what the story is about.`,
+      text_1: `I don't live close to city center but there is a bus stop right next to my home.
+                Although I live close to the bus stop I constantly miss it and then only option is to walk to the next stop which is at least 10 times further away.`,
+      text_2: `It is pretty hard to get up on time because you probably would like to spend more time in bed rather than to do something more productive.
+                After some time though I am up and running just to not miss my bus.
+                Coffee is also a very great thing that makes me just stop and make it, I usually use moka pot to make my coffee nowadays because it tastes fantastic.
+                And so it is, after sitting at the table I notice that my bus leaves after five minutes and I won't get there in time.`,
+      text_3: `After spilled coffee bus is missed. Now I run out the door to make it in time to the next stop.`,
       btn_back: 'Step Back',
       btn_home: 'To Home',
       btn_continue: 'Continue'
